08a5cae4be5e1137a205692459ffa94aa8791370
hj.tian <hj.tian@aftership.com>
2022-06-22 23:55:48 +0800

    feat: add doomemacs configuration and README
    add doomemacs configuration and README

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # doom-emacs-configuration
+
+Doom Emacs = Emacs + VIM
+
+## common
+
+- spc fr (recently file)
+- spc fp (private configuration)
+- spc pp (choice projects)
+- spc spc (fuzzy search file in current projects)
+- spc bb (search buffer in current projects buffers)
+
+## ui
+
+- spc ht (choice theme)
+- spc ti (toggle indent)
+- spc tm (toggle minimap)
+- spc t\*
+
+## evil
+
+### Ported vim plugins
+
+- (gc) evil-nerd-commenter
+- (gs) evil-easymotion
+- (gl) evil-lion
+- (s) evil-snipe
+- (S) evil-embrace and evil-surround
+
+### Custom Text Objects
+
+- =w W= words
+- =s= sentences
+- =p= paragraphs
+- =b= parenthesized blocks
+- =b ( ) { } [ ] < >= braces, parentheses and brackets
+- =' " `= quotes
+- =t= tags
+- =o= symbols
+
+And these are text objects added by this module:
+
+- =a= C-style function arguments (provided by ~evil-args~)
+- =B= any block delimited by braces, parentheses or brackets (provided by
+  ~evil-textobj-anyblock~)
+- =c= Comments
+- =f= For functions (but relies on the major mode to have sane definitions for
+  ~beginning-of-defun-function~ and ~end-of-defun-function~)
+- =g= The entire buffer
+- =i j k= by indentation (=k= includes one line above; =j= includes one line
+  above and below) (provided by ~evil-indent-plus~)
+- =q= For quotes (any kind)
+- =u= For URLs
+- =x= XML attributes (provided by ~exato~)
+
+## magit
+
+- spc gg
+- ll (git lg)
+- s (git add)
+- dd (git diff)
+- cc (git commit)
+- F (git pull)
+- p (git push)
+- q (exit)
+
+## python-mode
+
+- snippets (yasnippets-snippets doomemacs-snippets)
+- company (+lsp)
+- linter (python-flake8)
+- formater (black)
+- quickrun
+- test (pytest)
+
+## terminal
+
+- spc o[tT] (toggle [BIG] vterm)
+
+## help, it's very helpful; )
+
+- spc hv (help variable)
+- spc hf (help function)
+- spc hm (help module)
---
+++ config.el
@@ -0,0 +1,130 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Navy Tian"
+      user-mail-address "hj.tian@aftership.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 16 :weight 'Regular)
+     doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 15))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-molokai)
+;; (setq doom-theme 'doom-vibrant)
+;; (setq doom-theme 'doom-dracula)
+
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+
+
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq auto-save-default t
+      make-backup-files t)
+(setq-default history-length 1000)
+(setq create-lockfiles nil)
+
+(setq confirm-kill-emacs nil)
+
+(setq org-agenda-skip-scheduled-if-done t)
+(setq org-agenda-skip-deadline-if-done t)
+(setq projectile-project-search-path '("~/AfterShip/" "~/github/"))
+
+
+(setq +format-with-lsp nil)
+
+(setq flycheck-flake8-maximum-line-length 120)
+(setq-default flycheck-disabled-checkers '(python-pylint python-mypy pydocstyle python-pyright python-pycompile))
+
+(after! centaur-tabs
+  (setq centaur-tabs-style "wave"))
+
+(setq projectile-ignored-projects '("~/" "/tmp"))
+
+(after! company
+  (setq company-idle-delay 0.1
+        company-tooltip-align-annotations t
+        company-show-numbers t
+        company-minimum-prefix-length 1))
+(setq +lsp-company-backends '(:separate company-yasnippet company-capf))
+(setq company-backends '(:separate company-yasnippet company-capf))
+
+(setq lsp-enable-file-watchers nil)
+(setq lsp-file-watch-threshold nil)
+
+;; (setq yas-triggers-in-field t)
+
+; mapping
+(map! :leader
+      :desc "Quickrun buffer code"
+      "c r" #'(lambda() (interactive) (quickrun) (message "Successed: OK")))
+
+
+(define-key evil-motion-state-map (kbd "C-h") #'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-j") #'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-k") #'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-l") #'evil-window-right)
+
+(set-formatter! 'black "black -l 119 -q -")
+
